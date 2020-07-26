2af1314aa090c7abab7578bb56fa194dd8561109
alamhanz <alamhanz@gmail.com>
2020-07-26 11:59:22 +0700

    update day 2

--- src/good_example.py
+++ src/good_example.py
@@ -1,3 +1,6 @@
 saya=input('punya1 :')
 kamu=input('punya2 :')
+
+print(type(saya))
+print(type(kamu))
 print(saya+kamu," yes yes")
---
+++ src/testing1.py
@@ -0,0 +1,5 @@
+a=input('saya :')
+b=input('kamu :')
+a=int(a)
+b=int(b)
+print('saya ditambah kamu = ',a+b)
